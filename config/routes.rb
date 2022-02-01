863d72ae13945993a4b6681104e002217250d1a7
gaki-h <walter.thi.gks@gmail.com>
2022-02-01 19:34:44 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'prototypes/index'
   root to: "prototypes#index"
+  root  'users#index'
   resources :users, only: [:new, :edit, :update, :show]
   resources :prototypes do
     resources :comments, only: :create
